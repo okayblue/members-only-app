c5165a5f7661dac5029b1f4bfd3cfb271c83f4c4
okayblue <56899411+okayblue@users.noreply.github.com>
2023-04-07 15:00:01 -0400

    Create #index and #new actions for Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
-  before_action: :authenticate_user!, except: [:show, :index]
+  before_action :authenticate_user!, except: [:show, :index]
 
   def index
     @posts = Post.all
+  end
+  
+  def new
+    @post = current_user.posts.build
   end
 end
